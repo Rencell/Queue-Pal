80f0a23a88d245713138a499447b9855ddf9e646
rencell <rencelltobelonia67@gmail.com>
2025-01-02 23:59:13 +0800

    crud

--- staff/urls.py
+++ staff/urls.py
@@ -6,4 +6,6 @@ from staff import views
 urlpatterns = [
     path('', views.user_index.as_view(), name="staff_index"),
     path('<str:code>', views.room_view.as_view(), name="staff_queue_room"),
+    path('<str:code>/create', views.store_customer.as_view(), name="staff_create_customer"),
+    path('<str:code>/<int:pk>/delete', views.destroy_customer.as_view(), name="staff_delete_customer"),
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,7 +1,7 @@
 import random
 import string
 from django.shortcuts import redirect, render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import GroupRequiredMixin # type: ignore
 from django.urls import reverse_lazy
@@ -60,5 +60,32 @@ class room_view(GroupRequiredMixin, ListView):
         
         context['Room'] = Room.objects.get(code=uid)
         return context
+    
+    
+class store_customer(GroupRequiredMixin, CreateView):
+    
+    model= UserRoom
+    template_name = "staff/create_customer.html"
+    fields = ['user', 'room', 'status']
+    group_required = ['Staff', 'admin']
+    
+    def get_success_url(self):
+        return reverse_lazy('staff_queue_room', kwargs={'code': self.kwargs.get('code')})
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['code'] =  self.kwargs.get('code')
+        return context
+    
+class destroy_customer(GroupRequiredMixin, DeleteView):
+    
+    model= UserRoom
+    template_name = "staff/userroom_confirm_delete.html"
+    group_required = ['Staff', 'admin']
+    
+    def get_success_url(self):
+        return reverse_lazy('staff_queue_room', kwargs={'code': self.kwargs.get('code')})
+    
     
     
